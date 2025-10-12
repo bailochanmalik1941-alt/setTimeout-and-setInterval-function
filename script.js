734df53fe359af62012f81894c445d7731f1be80
Bailochan malik <bailochanmalik1941@gmail.com>
2025-10-12 22:12:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,15 @@ if (age > 18) {
 setTimeout(() => {
    console.log("you are a good preson ")
 }, 2000);
+
+// let age = prompt('enter yoour age');
+// if (age > 18) {
+//     console.log("you are seniour");
+// } else {
+//     console.log("your are juniour");
+//     console.log("you are a simple boy and handsome guy in your college ")
+// }
+// setInterval(() => {
+//    console.log("you are a good preson ")
+// }, 3000);
+
